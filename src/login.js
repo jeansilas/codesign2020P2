4cbf96c0a6a71eaa24e594c28decbb75fedb43cc
lhfaguiar <lais.harumi.f@gmail.com>
2020-11-28 23:34:01 -0300

    finaliza logins

--- src/login.js
+++ src/login.js
@@ -1,15 +1,21 @@
 function validar(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+
     var loginErrorMsg = document.getElementById("erro");
     if ( username == "usuario" && password == "senha"){
-        // alert ("Login successfully");
-        window.location = "inicio_professor.html"; // Redirecting to other page.
+        window.location = "inicio_professor.html";
         return false;
     }
     else{
-        location.reload()
-        loginErrorMsg.style.opacity = 1;
+        loginErrorMsg.className = "aparece";
+        // location.reload()
+        
         return false;
     }
+}
+
+
+function erro(){
+    window.location = "msg_erro.html";
 }
