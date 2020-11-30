b80301cff77a68c1960835ae46fd1d1463846bad
Karalos <carloseas1@al.insper.edu.br>
2020-11-30 15:23:57 -0300

    chat com jscript

--- src/chat.js
+++ src/chat.js
@@ -1,5 +1,10 @@
-function enviar_chat() {
-    window.location = "mensagens_comunidade.html";
-    return false;
+function enviar(){
+    var mensagem= document.getElementById("mensagem").value;
+    var ul= document.getElementById('ul');
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(mensagem));
+    li.className += "a-sub1";
+    ul.appendChild(li);
+    document.forms['form-chat'].reset()
+    return false; 
 }
-
