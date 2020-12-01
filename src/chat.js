64cd00295d8eff6c659fb61cbb9e5fa4ad611dd1
Karalos <carloseas1@al.insper.edu.br>
2020-12-01 20:27:02 -0300

    chat topico

--- src/chat.js
+++ src/chat.js
@@ -13,3 +13,35 @@ function enviar(){
         return false; 
     }
 }
+
+function enviar1(){
+    var mensagem= document.getElementById("mensagem1").value;
+    var ul= document.getElementById('ul1');
+    var li = document.createElement("li");
+    if( mensagem==''){
+        return false;
+    }
+    else{
+        li.appendChild(document.createTextNode(mensagem));
+        li.className += "azul";
+        ul.appendChild(li);
+        document.forms['form-chat1'].reset()
+        return false; 
+    }
+}
+
+function enviar2(){
+    var mensagem= document.getElementById("mensagem2").value;
+    var ul= document.getElementById('ul2');
+    var li = document.createElement("li");
+    if( mensagem==''){
+        return false;
+    }
+    else{
+        li.appendChild(document.createTextNode(mensagem));
+        li.className += "azul";
+        ul.appendChild(li);
+        document.forms['form-chat2'].reset()
+        return false; 
+    }
+}
